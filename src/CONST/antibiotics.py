ad9ff9d03388bca7fb85b71a80c14fecd7a172a3
Aërium Srīvus (ÆĒR ŚRĪV) <aeer.sriv@gmail.com>
2026-05-22 18:35:51 +0800

    constants for antibiotics and structures

---
+++ src/CONST/antibiotics.py
@@ -0,0 +1,86 @@
+class Ligands:
+    ANTIBIOTICS = [
+            "amoxicillin", # beta-lactams/penicillins
+            "piperacillin",
+            "ticarcillin",
+            "cefepime", # beta-lactams/cephalosporins
+            "cefazolin",
+            "ceftolozane",
+            "ceftriaxone",
+            "ceftazidime",
+            "aztreonam", # beta-lactams/monobactam
+            "imipenem", # beta-lactams/carbapenems
+            "meropenem",
+            "doripenem",
+            "vancomycin", # glycopeptide
+            "minocycline", # tetracyclines
+            "tigecycline",
+            "doxycycline",
+            "tetracycline",
+            "Chlortetracycline",
+            "azithromycin", # macrolides
+            "erythromycin",
+            "clarithromycin",
+            "chloramphenicol", # chlorampenicol
+            "linezolid", # oxazolidinones
+            "rifampicin", # rifamycin
+            "fosfomycin", # phosphonic acid
+            "amikacin", # aminoglycosides
+            "tobramycin",
+            "streptomycin",
+            "kanamycin",
+            "trimethoprim", # trimethoprim
+            "sulfamethoxazole", # sulfamethoxazole
+            "ciprofloxacin", # quinolones
+            "levofloxacin",
+            "moxifloxacin"
+        ]
+    AB_ARR = [
+        [
+            "amoxicillin", # penicillins
+            "piperacillin",
+            "ticarcillin"
+        ], [
+            "cefepime", # cephalosporins
+            "cefazolin",
+            "ceftolozane",
+            "ceftriaxone",
+            "ceftazidime",
+        ], [
+            "aztreonam" # monobactam
+        ], [
+            "imipenem", # carbapenems
+            "meropenem",
+            "doripenem"
+        ], [
+            "vancomycin" # glycopeptide
+        ], [
+            "minocycline", # tetracyclines
+            "tigecycline",
+            "doxycycline",
+            "tetracycline",
+            "Chlortetracycline"
+        ], [
+            "azithromycin", # macrolides
+            "erythromycin",
+            "clarithromycin"
+        ], [
+            "chloramphenicol" # chlorampenicol
+        ], [
+            "linezolid" # oxazolidinones
+        ], [
+            "amikacin", # aminoglycosides
+            "tobramycin",
+            "streptomycin",
+            "kanamycin"
+        ], [
+            "trimethoprim" # trimethoprim
+        ], [
+            "sulfamethoxazole" # sulfamethoxazole
+        ], [
+            "ciprofloxacin", # quinolones
+            "levofloxacin",
+            "Moxifloxacin"
+        ]
+    ]
+
