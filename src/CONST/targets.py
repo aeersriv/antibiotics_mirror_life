bd960b17792d5d49e1871fbc4e38c478093ad983
Aërium Srīvus (ÆĒR ŚRĪV) <aeer.sriv@gmail.com>
2026-05-24 03:29:25 +0800

    hashmap for target-ligand_arr

--- src/CONST/targets.py
+++ src/CONST/targets.py
@@ -1,6 +1,6 @@
 class Targets:
-    TARGETS: dict[str, list[str]] = {
-            "6I1H": [ # beta-lactams
+    ab_classes: dict[str, list[str]] = {
+        "beta_lactams": [ # beta-lactams
                 "amoxicillin", # penicillins
                 "piperacillin",
                 "ticarcillin",
@@ -14,50 +14,74 @@ class Targets:
                 "meropenem",
                 "doripenem"
             ],
-            "5M18": [
-                "amoxicillin", # penicillins
-                "piperacillin",
-                "ticarcillin",
-                "cefepime", # cephalosporins
-                "cefazolin",
-                "ceftolozane",
-                "ceftriaxone",
-                "ceftazidime",
-                "aztreonam", # monobactam
-                "imipenem", # carbapenems
-                "meropenem",
-                "doripenem"
-            ],
-            "1FVM": [
-                "vancomycin" # glycopeptide
-            ],
-            "8CF1": [ # 30S subunit
-                "minocycline", # tetracyclines
+        "glycopeptide": ["vancomycin"],
+        "tetracyclines": [
+                "minocycline",
                 "tigecycline",
                 "doxycycline",
                 "tetracycline",
-                "Chlortetracycline",
+                "chlortetracycline"
+            ],
+        "macrolides": [
+                "azithromycin", # macrolides
+                "erythromycin",
+                "clarithromycin"
+            ],
+        "chlorampennicol": [
+                "chlorampenicol"
+            ],
+        "oxazolidinones": [
+                "linezolid"
+            ],
+        "rifampicin": [
+                "rifampicin"
+            ],
+        "phosphonic": [
+                "fosfomycin"
+            ],
+        "aminoglycosides": [
                 "amikacin", # aminoglycosides
                 "tobramycin",
                 "streptomycin",
                 "kanamycin"
+
             ],
-            "8CGD": [ # 50S subunit
-                "azithromycin", # macrolides
-                "erythromycin",
-                "clarithromycin",
-                "chloramphenicol" # chlorampenicol
-            ],
-            "3FL9": [
+        "trimethoprim": [
                 "trimethoprim" # trimethoprim
             ],
-            "1TX2": [
+        "sulfonamide": [
                 "sulfamethoxazole" # sulfamethoxazole
             ],
-            "4DUH": [
+        "quinolones": [
                 "ciprofloxacin", # quinolones
                 "levofloxacin",
-                "Moxifloxacin"
+                "moxifloxacin"
             ]
+    }
+
+    TARGETS: dict[str, list[str]] = {
+            "6I1H": ab_classes["beta_lactams"],
+            "1FVM": ab_classes["glycopeptide"],
+            "8CF1": ab_classes["tetracyclines"],
+            "8CGD": (
+                    ab_classes["macrolides"] +
+                    ab_classes["chlorampennicol"]
+                ),
+            "1NJ1": (
+                    ab_classes["macrolides"] +
+                    ab_classes["chlorampennicol"]
+                ),
+            "7S1H": ab_classes["oxazolidinones"],
+            "1YNN": ab_classes["rifampicin"],
+            "1YBG": ab_classes["phosphonic"],
+            "4DUH": ab_classes["quinolones"],
+            "8C41": ab_classes["quinolones"],
+            "1FJG": ab_classes["aminoglycosides"],
+            "3FL9": ab_classes["trimethoprim"],
+            "1TX2": ab_classes["sulfonamide"],
+            "3TZF": ab_classes["sulfonamide"]
         }
 
+    AMR_TAR: dict[str, list[str]] = {
+            "5M18": ab_classes["beta_lactams"],
+        }
