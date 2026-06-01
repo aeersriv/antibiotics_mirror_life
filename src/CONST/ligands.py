606f0ec8e4334203e4f2c25e7d2a144c889fd972
Aërium Srīvus (ÆĒR ŚRĪV) <aeer.sriv@gmail.com>
2026-06-02 01:25:48 +0800

    ligand constants used in the study in array format

---
+++ src/CONST/ligands.py
@@ -0,0 +1,55 @@
+class Ligands:
+    LIG_ARR: list[list[str]] = [
+        [
+            "amoxicillin",
+            "piperacillin",
+            "ticarcillin"
+        ], [
+            "cefepime",
+            "cefazolin",
+            "ceftolozane",
+            "ceftriaxone",
+            "ceftazidime"
+        ], [
+            "aztreonam"
+        ], [
+            "imipenem",
+            "meropenem",
+            "doripenem"
+        ], [
+            "vancomycin"
+        ], [
+            "tigecycline"
+        ], [
+            "minocycline",
+            "doxycycline",
+            "tetracycline",
+            "chlortetracycline"
+        ],
+        [
+            "azithromycin",
+            "erythromycin",
+            "clarithromycin"
+        ], [
+            "chloramphenicol"
+        ], [
+            "linezolid"
+        ], [
+            "rifampicin"
+        ], [
+            "fosfomycin"
+        ], [
+            "amikacin",
+            "tobramycin",
+            "streptomycin",
+            "kanamycin"
+        ], [
+            "trimethoprim"
+        ], [
+            "sulfamethoxazole"
+        ], [
+            "ciprofloxacin",
+            "levofloxacin",
+            "moxifloxacin"
+        ]
+    ]
