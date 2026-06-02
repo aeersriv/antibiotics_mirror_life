246eb606c874780d75dea52dda51a79dc9614b88
Aërium Srīvus (ÆĒR ŚRĪV) <aeer.sriv@gmail.com>
2026-06-02 16:15:26 +0800

    implement logging on ligand preparation function ligand_prep()

--- src/utils/ligprep.py
+++ src/utils/ligprep.py
@@ -8,55 +8,63 @@ from subprocess import run
 from src.utils.logger import Logger
 from typing_extensions import Self
 
-def ligand_prep(
-        lig_path: str,
-        name: str,
-        path: str,
-        log_: Logger
-    ) -> str | None:
-    """_summary_
 
-    Args:
-        lig_path (str): Path of the ligand SDF.
-        name (str): File name for the scrubbed and prepped ligand.
-        path (str): Path where to save the ligand.
-        logger (Logger): Logger file.
+class LigPrep:
+    """Ligand Preparation Tool."""
 
-    Returns:
-        str | None: A successful preparation will return the name of
-            the ligand, whilst a failed will return None.
-    """
+    def __init__(self: Self, log_: Logger) -> None:
+        self.log_: Logger = log_
 
 
-    pH: float = 7.4
+    def ligand_prep(
+            self: Self,
+            lig_path: str,
+            name: str,
+            path: str,
+        ) -> str | None:
+        """_summary_
 
-    scrub_cmd: list[str] = [
-            "scrub.py",
-            lig_path,
-            "-o",
-            f"{path}/scrub/{name}-scrub.sdf",
-            "--ph", str(pH),
-            "--skip_tautomer"
-        ]
-    log_.info(f"Scrubing {name} with {' '.join(scrub_cmd)}")
-    _scrub_cmd_ = run(scrub_cmd)
+        Args:
+            lig_path (str): Path of the ligand SDF.
+            name (str): File name for the scrubbed and prepped ligand.
+            path (str): Path where to save the ligand.
+            logger (Logger): Logger file.
 
-    if _scrub_cmd_.returncode == 1:
-        log_.info(f"Failed to run MolScrub on {name}")
-        return None
+        Returns:
+            str | None: A successful preparation will return the name of
+                the ligand, whilst a failed will return None.
+        """
 
-    mkprepligand_cmd: list[str] = [
-            "mk_prepare_ligand.py",
-            "-i", f"{path}/scrub/{name}-scrub.sdf",
-            "-o", f"{path}/{name}-prepped.pdbqt"
-        ]
-    log_.info(
-        "Running mk_prepapre_ligand.py on "
-        f"{name} with {' '.join(mkprepligand_cmd)}"
-    )
-    _mkprepligand_cmd_ = run(mkprepligand_cmd)
 
-    if _mkprepligand_cmd_.returncode == 1:
-        return None
+        pH: float = 7.4
 
-    return name
+        scrub_cmd: list[str] = [
+                "scrub.py",
+                lig_path,
+                "-o",
+                f"{path}/scrub/{name}-scrub.sdf",
+                "--ph", str(pH),
+                "--skip_tautomer"
+            ]
+        self.log_.info(f"Scrubing {name} with {' '.join(scrub_cmd)}")
+        _scrub_cmd_ = run(scrub_cmd)
+
+        if _scrub_cmd_.returncode == 1:
+            self.log_.info(f"Failed to run MolScrub on {name}")
+            return None
+
+        mkprepligand_cmd: list[str] = [
+                "mk_prepare_ligand.py",
+                "-i", f"{path}/scrub/{name}-scrub.sdf",
+                "-o", f"{path}/{name}-prepped.pdbqt"
+            ]
+        self.log_.info(
+            "Running mk_prepapre_ligand.py on "
+            f"{name} with {' '.join(mkprepligand_cmd)}"
+        )
+        _mkprepligand_cmd_ = run(mkprepligand_cmd)
+
+        if _mkprepligand_cmd_.returncode == 1:
+            return None
+
+        return name
