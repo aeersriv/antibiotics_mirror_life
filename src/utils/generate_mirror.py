955a28360fde3bd80eaf1889b836ebd734c033e9
Aërium Srīvus (ÆĒR ŚRĪV) <aeer.sriv@gmail.com>
2026-06-02 16:24:54 +0800

    deprecate unused invert_mol() and implement logging in invert_pdb()

--- src/utils/generate_mirror.py
+++ src/utils/generate_mirror.py
@@ -12,50 +12,44 @@
 # This allows it to be used as an import for further processing
 # such as in Jupyter notebooks or other scripts.
 
-from rdkit.Chem.rdchem import Mol
+from typing_extensions import Self
+
+from src.utils.logger import Logger
 
 
-def invert_mol(native_mol: Mol) -> Mol:
-    """Generate enantiomorph of the native molecule.
+class MirrorTarget:
+    """Mirror the structure of target."""
 
-    Args:
-        native_mol (Mol): RDkit.Chem.rdchem.Mol instance
-
-    Returns:
-        Mol: RDkit.Chem.rdchem.Mol instance instance of
-        mirror of the native mol
-    """
-    mirror_mol = native_mol
-    for atom in native_mol.GetAtoms():
-        atom.InvertChirality()
-
-    return mirror_mol
+    def __init__(self: Self, log_: Logger) -> None:
+        self.log_: Logger = log_
 
 
-def invert_pdb(native_pdb: str, path: str) -> None:
-    """Read a PDB file, and manually invert the sign of
-    each x, y, and z coordinates in HETATM and ATOM lines.
+    def invert_pdb(self: Self, pdbid: str, path: str) -> None:
+        """Read a PDB file, and manually invert the sign of
+        each x, y, and z coordinates in HETATM and ATOM lines.
 
-    Args:
-        native_pdb (str): file path of native target.
-        path (str): output where to save the mirror target.
-    """
+        Args:
+            pdbid (str): file path of native target.
+            path (str): output where to save the mirror target.
+        """
 
-    mirror_pdb = f"{path}/L-{native_pdb.split('/')[-1]}"
-    with (
-            open(native_pdb, "r", encoding="utf-8") as d_pdb,
-            open(mirror_pdb, "w", encoding="utf-8") as l_pdb
-        ):
-        for line in d_pdb:
-            if line.startswith("HETATM") or line.startswith("ATOM"):
-                # invert the signs to transpose the coordinates of
-                # the given peptide structure to "mirror form."
-                x = -1.0 * float(line[30:38])
-                y = -1.0 * float(line[38:46])
-                z = -1.0 * float(line[46:54])
-                newline = f"{line[:30]}{x:8.3f}{y:8.3f}{z:8.3f}{line[54:]}"
-                # line containing coordinates with inverted signs
-                l_pdb.write(newline)
-            else:
-                l_pdb.write(line)
+        self.log_.info(f"Inverting the structure of {pdbid} ...")
+        mirror_pdb = f"{path}/L-{pdbid.split('/')[-1]}"
+        with (
+                open(pdbid, "r", encoding="utf-8") as d_pdb,
+                open(mirror_pdb, "w", encoding="utf-8") as l_pdb
+            ):
+            for line in d_pdb:
+                if line.startswith("HETATM") or line.startswith("ATOM"):
+                    # invert the signs to transpose the coordinates of
+                    # the given peptide structure to "mirror form."
+                    x = -1.0 * float(line[30:38])
+                    y = -1.0 * float(line[38:46])
+                    z = -1.0 * float(line[46:54])
+                    newline = f"{line[:30]}{x:8.3f}{y:8.3f}{z:8.3f}{line[54:]}"
+                    # line containing coordinates with inverted signs
+                    l_pdb.write(newline)
+                else:
+                    l_pdb.write(line)
 
+        self.log_.info(f"Saved mirror of {pdbid} as {mirror_pdb}.")
